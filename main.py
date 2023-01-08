3fe6e1c12b6071ff83408c9f08e4bd436030aec5
Alapan <alapandas333@gmail.com>
2023-01-08 13:37:32 +0530

    Complete

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 # def hello_world(request):
-import os,telebot
-import requests, json
+import os
+import telebot
+import requests
+import json,datetime
 
-API_KEY='5978028781:AAHo8vS2cIYKTamNbvCHNvIoYb1UcaZHkQI'
-bot=telebot.TeleBot(API_KEY)
-# while True:
+API_KEY = '5978028781:AAHo8vS2cIYKTamNbvCHNvIoYb1UcaZHkQI'
+bot = telebot.TeleBot(API_KEY)
 
 @bot.message_handler(commands=['start'])
 def show(message):
-    bot.reply_to(message,"Hello! Welcome to GCECT")
+    bot.reply_to(message, "Hello! Welcome to GCECT")
+
 
 @bot.message_handler(commands=['helpline'])
 def show(message):
-    bot.reply_to(message,"""following commands will perform as stated below:
+    bot.reply_to(message, """following commands will perform as stated below:
         /start -> Welcome to our college
         /helpline -> this message will show
         /content -> About the various weather details of different regions
@@ -22,14 +24,16 @@ def show(message):
         /pressure -> showing atm pressure
         /wind_speed -> showing wind speed""")
 
+
 @bot.message_handler(commands=['content'])
 def show(message):
-    bot.reply_to(message,""" We have various details regarding weather available :
+    bot.reply_to(message, """ We have various details regarding weather available :
                 basic weather details
                 sunrise
                 sunset
                 air pressure
-                wind speed""")    
+                wind speed""")
+
 
 @bot.message_handler(func=lambda message: True)
 def weather(message):
@@ -37,32 +41,37 @@ def weather(message):
 
     W_Url = "https://api.openweathermap.org/data/2.5/weather?"
     API_KEY = '4961dfe8dac0eb120ff56f0dd8af8f0a'
-    URL = W_Url + "q=" +c+ "&appid=" + API_KEY
+    URL = W_Url + "q=" + c + "&appid=" + API_KEY
 
-    City=""
+    City = ""
     response = requests.get(URL)
 
-    #---->Condition request
+    # ---->Condition request
     if response.status_code == 200:
-        #---->formating to Json
+        # ---->formating to Json
         data = response.json()
-        #---->gettingt ta dictionary
+        # ---->gettingt ta dictionary
         main = data['main']
-        #----->Temperature
+        # ----->Temperature
         temp = (int(main['temp'])-273.0)
-        #----->Visibilty
+        # ----->Visibilty
         visibility = (data['visibility'])
-        #---->Humidity
+        # ---->Humidity
         humid = (main['humidity'])
-        #---->Status
+        # ---->Status
         status = data['weather']
+        sunrise = datetime.datetime.fromtimestamp(data['sys']['sunrise'])
+        sunset = datetime.datetime.fromtimestamp(data['sys']['sunset'])
 
-        City+=f"-----{c}-----"+'\n'+f"Temperature: {temp} Celsius"+'\n'+f"Humidity: {humid} g/kg"+'\n'+f"Visibility: {visibility} "+'\n'+f"Weather Condition: {status[0]['description']}  :)"
-        
-        bot.send_message(message.chat.id,City)
+        City += f"-----{c}-----"+'\n'+f"Temperature: {temp} Celsius"+'\n'+f"Humidity: {humid} g/kg" + \
+            '\n'+f"Visibility: {visibility} "+'\n' +f"Sunrise {sunrise}"+'\n'+f"Sunset {sunset}"+'\n'+ \
+                f"Weather Condition: {status[0]['description']}"
+
+        bot.send_message(message.chat.id, City)
     else:
-        #Invalid City message
-        bot.send_message(message.chat.id,"""Enter  a  Valid City""")
+        # Invalid City message
+        bot.send_message(message.chat.id, """Enter  a  Valid City""")
+
 
 bot.infinity_polling()
 #   return 'OK'
